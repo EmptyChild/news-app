04c0a74f2eccce957e1d5a3107dca44d896a9449
Nikita Mikhaliuk <nikkita1992@mail.ru>
2018-04-17 15:47:10 +0300

    Additional messages on null search results in Main component

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -18,18 +18,27 @@ export default class Main extends Component {
     (
       <Spinner />
     ) : null;
-    const noMoreArticlesMessage = noMoreArticles && !fetchingArticles ? 
+    const noMoreArticlesMessage = articles.length && noMoreArticles && !fetchingArticles ? 
     (
       <div className='main__no-more-articles-message'>
-        No more Articles
+        No more Articles found
       </div>
     ) : null;
-    const searchResultMessage = filter ?
-    (
-      <div className='main__search-results-message'>
-        {`Search results for "${filter}"`}
-      </div>
-    ) : null;
+    const searchResultMessage = fetchingArticles ?
+      null
+      : !filter ?
+      null 
+      : articles.length ?
+      ( 
+        <div className='main__search-results-message'>
+          {`Search results for "${filter}"`}
+        </div>
+      )
+      : ( 
+        <div className='main__search-results-message'>
+          {` No search results for "${filter}"`}
+        </div>
+      );
     return (
       <main className='main'>
         <div className='wrapper'>
